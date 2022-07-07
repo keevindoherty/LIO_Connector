2d109314de87755359f710ad82bc22de0fcec191
Kevin <kdoherty@mit.edu>
2022-07-07 16:10:04 -0400

    Update viz script with notes.

--- scripts/viz_all_robots.py
+++ scripts/viz_all_robots.py
@@ -2,3 +2,7 @@ import g2o_to_o3d
 
 if __name__ == "__main__":
     print("Hello world")
+
+    # 1. Load each graph and optimized values
+    # 2. Load inter robot loop closures
+    # 3. Modify g2o_to_o3d function to handle multiple robots (keys "a", "b", "c", etc)
